e37b06d1d9c438c3d70b8e8acf1737e21692c02a
Abhinav Gupta <gabhinav001@gmail.com>
2023-09-10 14:30:14 -0400

    taks1: Complete modifications

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/pasithea/Desktop/ms/Cloud Computing/e6156-microservice-1/resources/students.json"
 
     def __init__(self):
         self.students = None
